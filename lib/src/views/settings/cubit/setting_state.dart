cd36167baac35048622dd9c9b6d8758a64f0f7ab
Thai Pham <thaibach2000@gmail.com>
2023-05-17 04:34:19 +0000

    fix khoang cach term of use

--- lib/src/views/settings/cubit/setting_state.dart
+++ lib/src/views/settings/cubit/setting_state.dart
@@ -3,3 +3,7 @@ abstract class SettingsState {}
 class SettingsLoading extends SettingsState {}
 
 class SettingsSuccess extends SettingsState {}
+
+class SettingSLanguageSuccess extends SettingsState{
+  
+}
